a014636126cf230b62fe695aa582f22b92389c0b
Moemen Saad <moemen.saad@nuance.com>
2021-05-30 00:22:51 +0200

    Changing folders structure

---
+++ src/Insurance.Domain/ProductTypeSurcharge.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Insurance.Domain
+{
+    public class ProductTypeSurchargeRate
+    {
+        [Key]
+        public int ProductTypeId { get; set; }
+        public float SurchargeRate { get; set; }
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+    }
+}
